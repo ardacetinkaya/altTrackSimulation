f5c95e0983599b33b25a4d6f5363a859e7d29351
ardacetinkaya <ardacetinkaya@gmail.com>
2019-04-09 08:04:03 +0300

    Improvements of vehicle sim.

--- altTrack.Vehicle/Vehicle.cs
+++ altTrack.Vehicle/Vehicle.cs
@@ -18,28 +18,43 @@ namespace altTrack.Vehicle
     public static class Vehicle
     {
         [FunctionName("Vehicle")]
-        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log,IConfiguration config)
+        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
-
-            List<string> _vehicles = new List<string>()
-            {
-                "YS2R4X20005399401",
-                "VLUR4X20009093588",
-                "VLUR4X20009048066",
-                "YS2R4X20005388011",
-                "YS2R4X20005387949",
-                "VLUR4X20009048096",
-                "YS2R4X20005387055"
-            };
-
-            var random = new Random();
-            var index = random.Next(_vehicles.Count);
-
-            var vehicleSimulation = _vehicles[index];
+            string vehicleSimulation = string.Empty;
 
             try
             {
+                //Get config file info for some configuration parameters
+                var config = new ConfigurationBuilder().SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables()
+                .Build();
+
+                //Vehicle set for simulation
+                List<string> _vehicles = new List<string>()
+                {
+                    "YS2R4X20005399401",
+                    "VLUR4X20009093588",
+                    "VLUR4X20009048066",
+                    "YS2R4X20005388011",
+                    "YS2R4X20005387949",
+                    "VLUR4X20009048096",
+                    "YS2R4X20005387055"
+                };
+
+                //Choose randomly a  vehicle
+                var random = new Random();
+                var index = random.Next(_vehicles.Count);
+
+                vehicleSimulation = _vehicles[index];
+
+
                 log.LogInformation($"Ping Service: {config["AltTrackPingService"]}");
+
+                //Just Ping the base(headquarters)
+                //No need to check result or do some other thing
+                //If any problem occurs, the next run of function do the job
+                //Like a simulation of a vehicle
                 HttpClient client = new HttpClient()
                 {
                     BaseAddress = new Uri(config["AltTrackPingService"])
@@ -51,7 +66,7 @@ namespace altTrack.Vehicle
                     }), Encoding.UTF8, "application/json"));
 
                 log.LogInformation($"Vehicle({vehicleSimulation}) simulation executed at: {DateTime.Now}");
-             
+
             }
             catch (Exception ex)
             {
